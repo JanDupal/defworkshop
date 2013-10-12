eb511ce7770b496471e48b395bada26e50826bc4
Jan Dupal <dupal.j@gmail.com>
2013-10-12 15:16:12 +0200

    State to recursion impl

--- src/defworkshop/state_to_recursion.clj
+++ src/defworkshop/state_to_recursion.clj
@@ -3,60 +3,82 @@
 
 ;; More often than not, state can be avoided by using recursion. Let's see how it works
 
-(defn ^:not-implemented reverse-coll
+(defn reverse-coll
   "Reverse the collection `coll`.
 
    You can use `loop/recur` construct to loop over the sequence.
    `cons` prepends items to the list, try that out."
   [coll]
-  (…))
+  (loop [c coll res []]
+    (if (empty? c)
+      res
+      (recur (rest c) (cons (first c) res)))))
 
-(defn ^:not-implemented recursive-sum
+(defn recursive-sum
   "We've already implemented sum using reduce, now let's move to implementing it via recursion!"
   [[head & tail]]
-  (…))
+  (if (empty? tail)
+      (or head 0)
+      (+ head (recursive-sum tail))))
 
-(defn ^:not-implemented recursive-sum-tc
+(defn recursive-sum-tc
   "with a tail-recursive version of sum, we can avoid stack overflows."
   ([coll]
-     (recursive-sum-tc coll 0))
+    (recursive-sum-tc coll 0))
   ([[head & tail] acc]
-     (…)))
+    (if (empty? tail)
+        (+ head acc)
+        (recursive-sum-tc tail (+ head acc)))))
 
-(defn ^:not-implemented max-from-list
+(defn max-from-list
   "Get the maximum from list using recursion"
   [[head & tail]]
   (if (empty? tail)
     head
-    (…)))
+    (let [max (max-from-list tail)]
+         (if (> head max) head max))))
 
-(defn ^:not-implemented my-reduce
+(defn my-reduce
   "generalizing the recursive sum example, write your own implementation of reduce! (for empty coll, just return nil.)"
   ([f [head & tail]]
      (my-reduce f head tail))
   ([f acc-init coll]
-     (…)))
+    (if (empty? coll)
+        acc-init
+        (my-reduce f (f acc-init (first coll)) (rest coll)))))
 
-(defn ^:not-implemented max-from-list-tc
+(defn max-from-list-tc
   "Get the maximum from list using tail recursion (avoid stack overflow)"
   ([coll]
-     (max-from-list-tc coll 0))
-  ([[head & tail] max]
-     (…)))
+     (if (empty? coll)
+         nil
+         (max-from-list-tc coll 0)))
+  ([[head & tail] m]
+     (if (empty? tail)
+         (max m head)
+         (max-from-list-tc tail (max m head)))))
 
-(defn ^:not-implemented loop-recur-sum
+(defn loop-recur-sum
   "This implementation is somewhat easier to understand for people coming from imperative style."
   [numbers]
-  (…))
+  (loop [coll numbers s 0]
+    (if (empty? coll)
+        s
+        (recur (rest coll) (+ s (first coll))))))
 
-(defn ^:not-implemented map-from-keys-and-vals
+(defn map-from-keys-and-vals
   "Something that we've already implemented previously in terms of `zipmap`, but are going to implement again
    in terms of recursion. Usually you use `loop/recur` construct whenever you have a one or multiple accumulators
    or several collections you iterate over."
   [keys vals]
-  (…))
+  (loop [ks keys vs vals m {}]
+    (if (or (empty? ks) (empty? vs))
+        m
+        (recur (rest ks)
+               (rest vs)
+               (assoc m (first ks) (first vs))))))
 
-(defn ^:not-implemented parentheses-balanced?
+(defn parentheses-balanced?
   "Check wether the given string has balanced parantheses or no.
 
    You can use `cond` statement to avoid deeply nested ints.
@@ -65,4 +87,8 @@
    `inc` increments a number, `dec` decrements a number."
   ([str] (parentheses-balanced? str 0))
   ([[current & tail] count]
-     (…)))
+    (cond
+      (nil? current) (= count 0)
+      (= current \() (parentheses-balanced? tail (inc count))
+      (= current \)) (parentheses-balanced? tail (dec count))
+      :else (parentheses-balanced? tail count))))
